de0741dacbf0b1d7256fb8d06655f4de0dfeb211
daoanhkhoa123 <158530319+daoanhkhoa123@users.noreply.github.com>
2025-10-24 21:28:51 +0700

    add folder aasist

---
+++ src/aasist/main.py
@@ -0,0 +1,12 @@
+import warnings
+from importlib import import_module
+
+def get_model(model_config, device):
+    """Define DNN model architecture"""
+    module = import_module("src.aasist.models.{}".format(model_config["architecture"]))
+    _model = getattr(module, "Model")
+    model = _model(model_config).to(device)
+    nb_params = sum([param.view(-1).size()[0] for param in model.parameters()])
+    print("no. model params:{}".format(nb_params))
+
+    return model
